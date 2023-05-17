7b188d330c488fbf949cddb28c19e780dcc690fd
kamorisan <kamori@redhat.com>
2023-05-17 11:36:21 +0900

    updates

---
+++ solution/java/Dbsync.java
@@ -0,0 +1,47 @@
+// camel-k: language=java dependency=camel-atlasmap
+
+import org.apache.camel.builder.RouteBuilder;
+
+public class Direct extends RouteBuilder {
+  @Override
+  public void configure() throws Exception {
+
+    from("kamelet:kafka-source?" 
+        + "topic=debezium.public.products&"
+        + "bootstrapServers=kafka-cluster-kafka-bootstrap.user1-dev.svc:9092&"
+        + "securityProtocol=PLAINTEXT&"
+        + "user=demo&"
+        + "password=demo&"
+        + "autoOffsetReset=earliest")
+    .to("atlasmap:file:/etc/camel/resources/atlasmap-mapping.adm")
+    .choice()
+        .when(simple("${body.contains(\"op\":\"u\")}"))
+            .log("UPDATE: ${body}")
+            .to("kamelet:postgresql-sink?" 
+                + "serverName=postgresql-replica.user1-dev.svc.cluster.local&" 
+                + "serverPort=5432&" 
+                + "username=demo&" 
+                + "password=demo&" 
+                + "query=UPDATE products SET name=:#name where id=:#id&" 
+                + "databaseName=sampledb")
+        .when(simple("${body.contains(\"op\":\"d\")}"))
+            .log("DELETE: ${body}")
+            .to("kamelet:postgresql-sink?" 
+                + "serverName=postgresql-replica.user1-dev.svc.cluster.local&" 
+                + "serverPort=5432&" 
+                + "username=demo&" 
+                + "password=demo&" 
+                + "databaseName=sampledb&" 
+                + "query=DELETE from products where id=:#id")
+        .otherwise()
+            .log("Otherwise")
+            .to("kamelet:postgresql-sink?" 
+                + "serverName=postgresql-replica.user1-dev.svc.cluster.local&" 
+                + "serverPort=5432&" 
+                + "username=demo&" 
+                + "password=demo&" 
+                + "databaseName=sampledb&" 
+                + "query=INSERT INTO products (id, name) VALUES (:#id, :#name)");
+      
+  }
+}
--- solution/java/PostgreSQL.java
+++ solution/java/PostgreSQL.java
@@ -13,9 +13,20 @@ public class PostgreSQL extends RouteBuilder {
 
       from("timer:java?repeatCount=1")
         .setBody().constant("{\"id\":4, \"name\":\"melon\"}")
-        .to("kamelet:postgresql-sink?serverName=postgresql.user1-dev.svc.cluster.local&serverPort=5432&username=demo&password=demo&query=insert into products (id, name) values (:#id, :#name)&databaseName=sampledb")
+        .to("kamelet:postgresql-sink?"
+          + "serverName=postgresql.user1-dev.svc.cluster.local&"
+          + "serverPort=5432&"
+          + "username=demo&"
+          + "password=demo&"
+          + "query=insert into products (id, name) values (:#id, :#name)&"
+          + "databaseName=sampledb")
         .marshal().json(JsonLibrary.Jackson)
-        .to("kamelet:postgresql-sink?serverName=postgresql.user1-dev.svc.cluster.local&serverPort=5432&username=demo&password=demo&query=select * from products&databaseName=sampledb")
+        .to("kamelet:postgresql-sink?"
+          + "serverName=postgresql.user1-dev.svc.cluster.local&"
+          + "serverPort=5432&"
+          + "username=demo&"
+          + "password=demo&"
+          + "query=select * from products&databaseName=sampledb")
         .log("${body}");
       
   }
