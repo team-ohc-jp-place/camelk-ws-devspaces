c660128196633371d35fe12c7df95e71f74ef3da
kamorisan <kamori@redhat.com>
2023-09-19 15:38:17 +0900

    updates

--- solution/java/Dbsync.java
+++ solution/java/Dbsync.java
@@ -1,47 +1,50 @@
-// camel-k: language=java dependency=camel-atlasmap
+// camel-k: language=java
 
 import org.apache.camel.builder.RouteBuilder;
 
 public class Dbsync extends RouteBuilder {
-  @Override
-  public void configure() throws Exception {
-
-    from("kamelet:kafka-source?" 
-        + "topic=debezium.public.products&"
-        + "bootstrapServers=kafka-cluster-kafka-bootstrap.user1-dev.svc:9092&"
-        + "securityProtocol=PLAINTEXT&"
-        + "user=demo&"
-        + "password=demo&"
-        + "autoOffsetReset=earliest")
-    .to("atlasmap:file:/etc/camel/resources/atlasmap-mapping.adm")
-    .choice()
-        .when(simple("${body.contains(\"op\":\"u\")}"))
-            .log("UPDATE: ${body}")
-            .to("kamelet:postgresql-sink?" 
-                + "serverName=postgresql-replica.user1-dev.svc.cluster.local&" 
-                + "serverPort=5432&" 
-                + "username=demo&" 
-                + "password=demo&" 
-                + "query=UPDATE products SET name=:#name where id=:#id&" 
-                + "databaseName=sampledb")
-        .when(simple("${body.contains(\"op\":\"d\")}"))
-            .log("DELETE: ${body}")
-            .to("kamelet:postgresql-sink?" 
-                + "serverName=postgresql-replica.user1-dev.svc.cluster.local&" 
-                + "serverPort=5432&" 
-                + "username=demo&" 
-                + "password=demo&" 
-                + "databaseName=sampledb&" 
-                + "query=DELETE from products where id=:#id")
-        .otherwise()
-            .log("CREATE: ${body}")
-            .to("kamelet:postgresql-sink?" 
-                + "serverName=postgresql-replica.user1-dev.svc.cluster.local&" 
-                + "serverPort=5432&" 
-                + "username=demo&" 
-                + "password=demo&" 
-                + "databaseName=sampledb&" 
-                + "query=INSERT INTO products (id, name) VALUES (:#id, :#name)");
-      
-  }
+    @Override
+    public void configure() throws Exception {
+        from("kamelet:kafka-source?"
+            + "topic=debezium.public.products&"
+            + "bootstrapServers=kafka-cluster-kafka-bootstrap.user1-dev.svc:9092&"
+            + "securityProtocol=PLAINTEXT&"
+            + "user=demo&"
+            + "password=demo&"
+            + "autoOffsetReset=earliest")
+            .routeId("route-b00a")
+            .setBody(jsonpath("$.payload"))
+            .choice()
+                .when(simple("${body['__op']} == 'u'"))
+                    .log("Update: ${body}")
+                    .marshal().json()
+                    .to("kamelet:postgresql-sink?"
+                        + "serverName=postgresql-replica.user1-dev.svc.cluster.local&"
+                        + "serverPort=5432&"
+                        + "username=demo&"
+                        + "password=demo&"
+                        + "query=UPDATE products SET name=:#name where id=:#id&"
+                        + "databaseName=sampledb")
+                .when(simple("${body['__op']} == 'd'"))
+                    .log("Delete: ${body}")
+                    .marshal().json()
+                    .to("kamelet:postgresql-sink?"
+                        + "serverName=postgresql-replica.user1-dev.svc.cluster.local&"
+                        + "serverPort=5432&"
+                        + "username=demo&"
+                        + "password=demo&"
+                        + "query=DELETE from products where id=:#id&"
+                        + "databaseName=sampledb")
+                .otherwise()
+                    .log("Create: ${body}")
+                    .marshal().json()
+                    .to("kamelet:postgresql-sink?"
+                        + "serverName=postgresql-replica.user1-dev.svc.cluster.local&"
+                        + "serverPort=5432&"
+                        + "username=demo&"
+                        + "password=demo&"
+                        + "query=INSERT INTO products (id, name) VALUES (:#id, :#name)&"
+                        + "databaseName=sampledb")
+            .endChoice();
+    }
 }
