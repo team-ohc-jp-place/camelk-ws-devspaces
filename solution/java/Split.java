c8aebee14bc2b4a9b7dfd49ee490c08752fb193a
kamori <kamori@redhat.com>
2024-08-07 15:06:51 +0900

    update

--- solution/java/Split.java
+++ solution/java/Split.java
@@ -1,19 +1,17 @@
 // camel-k: language=java
 // camel-k: dependency=camel-csv
-// camel-k: dependency=camel-jackson
 
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.dataformat.csv.CsvDataFormat;
-import org.apache.camel.model.dataformat.JsonLibrary;
 
 public class Split extends RouteBuilder {
   @Override
   public void configure() throws Exception {
 
-      from("file:data/input")
+      from("timer:split-pattern?period=1000&repeatCount=1")
+        .setBody(simple("id,name\n1,apple\n2,orange\n3,lemon"))
         .unmarshal(new CsvDataFormat().setUseMaps(true).setDelimiter(','))
-        .marshal().json(JsonLibrary.Jackson)
-        .split().jsonpath("$").parallelProcessing()
+        .split(body()).parallelProcessing()
         .log("${body}");
       
   }
