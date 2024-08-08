6303831501c5f87cce43afe410d6c9ceafaa3e60
kamori <kamori@redhat.com>
2024-08-08 14:20:56 +0900

    update

--- solution/java/Direct.java
+++ solution/java/Direct.java
@@ -7,9 +7,11 @@ public class Direct extends RouteBuilder {
   public void configure() throws Exception {
 
     from("timer:java?repeatCount=1")
+      .routeId("first-route")
       .to("direct:secondRoute");
     
     from("direct:secondRoute")
+      .routeId("second-route")
       .log("secoundRoute invoked");
       
   }
