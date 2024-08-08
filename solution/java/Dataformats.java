32c693c4b02ed739d59ebb9e2b897b619cfb1313
kamori <kamori@redhat.com>
2024-08-08 16:00:02 +0900

    updates

--- solution/java/Dataformats.java
+++ solution/java/Dataformats.java
@@ -10,7 +10,8 @@ public class Dataformats extends RouteBuilder {
   @Override
   public void configure() throws Exception {
 
-      from("file:data/input")
+      from("timer:split-pattern?period=1000&repeatCount=1")
+        .setBody(simple("id,name\n1,apple\n2,orange\n3,lemon"))
         .unmarshal(new CsvDataFormat().setUseMaps(true).setDelimiter(','))
         .marshal().json(JsonLibrary.Jackson)
         .log("${body}");
--- solution/java/Recipientlist.java
+++ solution/java/Recipientlist.java
@@ -10,17 +10,18 @@ public class Recipientlist extends RouteBuilder {
   @Override
   public void configure() throws Exception {
 
-      from("file:data/input")
-        .recipientList(simple("${bodyAs(String)}"),";");
+    from("timer:recipientlist?repeatCount=1")
+      .setHeader("destination", constant("direct:route-a;direct:route-c"))
+      .recipientList(header("destination"), ";");
 
-      from("direct:a")
-        .log("direct:a invoked");
+    from("direct:route-a")
+      .log("route-a invoked");
+
+    from("direct:route-b")
+      .log("route-b invoked");
       
-      from("direct:b")
-        .log("direct:b invoked");
-      
-      from("direct:c")
-        .log("direct:c invoked");
+    from("direct:route-c")
+      .log("route-c invoked");
       
   }
 }
